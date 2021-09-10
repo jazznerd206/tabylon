72bf8d930f9488906915b1bf0417eb498b35b0b0
Andrew Miller <206alm@gmail.com>
2021-09-09 18:56:25 -0700

    initial engine built

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.join(__dirname, 'public', 'index.html' )}),
+    new MiniCssExtractPlugin()
+  ]
+};
